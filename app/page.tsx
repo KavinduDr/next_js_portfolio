74464386564d29bbf4c19cd392d76deec48e6400
KavinduDr <rmkavindudhananjaya@gmail.com>
2024-12-27 18:36:47 +0530

    Enhance Certifications component with autoplay functionality and update package dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
           <Hero />
           <Grid />
           <MyProjects />
-          <Clients />
+          {/* <Clients /> */}
           <Certifications />
           {/* <Experience /> */}
           {/* <Approach /> */}
